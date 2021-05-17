35929074cd1a62c6e4e278640042ad1b996b2241
GhostPlays880 <80477077+GhostPlays880@users.noreply.github.com>
2021-05-17 12:15:28 -0400

    Added more walls

--- PacWorld.java
+++ PacWorld.java
@@ -18,11 +18,22 @@ public class PacWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 700, 1);
         
-        addObject(new WallFinal (), 350, 400 );
-        addObject(new WallFinal (), 215, 259 );
-        addObject(new WallFinal (), 498, 257);
-        addObject(new Wall2 (), 450, 340 );
-        addObject(new Wall2 (), 250, 340 );
+        addObject(new WallSmall (), 408, 407 );
+        addObject(new WallSmall (), 297, 408 );
+        addObject(new WallSmall (), 215, 259 );
+        addObject(new WallSmall (), 498, 257);
+        addObject(new WallSmall (), 216, 485);
+        addObject(new WallSmall (), 498, 493);
+        addObject(new WallTall (), 450, 340 );
+        addObject(new WallTall (), 250, 340 );
+        addObject(new WallTall (), 540, 411 );
+        addObject(new WallTall (), 174, 404 );
+        addObject(new WallTall (), 357, 492 );
+        addObject(new WallTall (), 693, 15);
+        addObject(new WallTall (), 693, 170);
+        addObject(new WallTall (), 693, 320);
+        addObject(new WallTall (), 693, 460);
+        addObject(new WallTall (), 693, 610);
     }
         }
 
--- WallSmall.java
+++ WallSmall.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Wall2 here.
+ * Write a description of class WallFinal here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Wall2 extends Actor
+public class WallSmall extends Actor
 {
     /**
-     * Act - do whatever the Wall2 wants to do. This method is called whenever
+     * Act - do whatever the WallFinal wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
